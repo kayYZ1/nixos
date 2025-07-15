b1ed54e5f855d3f9c09fa14d454084e5dde12115
kayYZ1 <overek7133@gmail.com>
2025-07-15 19:59:22 +0200

    dev

--- home/packages/development.nix
+++ home/packages/development.nix
@@ -2,23 +2,6 @@
 
 {
   home.packages = with pkgs; [
-    gcc
-    gdb
-    cmake
-    ninja
-    clang
-    clang-tools
-    cppcheck
-    valgrind
-
-    nodejs_latest
-    bun
-    deno
-    typescript
-    nodePackages.typescript-language-server
-    nodePackages.prettier
-    nodePackages.eslint
-
     nixpkgs-fmt
     nixd
     alejandra
